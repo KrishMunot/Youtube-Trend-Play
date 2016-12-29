1ed0fea8c277d6be29e8ad763a329da3f5b55561
Krish Munot <krishmunot@gmail.com>
2016-12-29 19:26:27 +0530

    add choices

--- ytrend.py
+++ ytrend.py
@@ -1,3 +1,4 @@
+
 #Play Trending Videos
 
 from bs4 import BeautifulSoup
@@ -14,16 +15,27 @@ soup = BeautifulSoup(data,"lxml")
 
 i=0 
 con = True
-while con==True:
+while con == True:
       list1=[]
       for link in soup.find_all("h3",{"class":"yt-lockup-title"}):
           for var in link.find_all("a"):
               title = var.get("title")
-              href  = var.get("href")
+              href = var.get("href")
               print("Title[%s] = %s\n"%(i,title))
               list1.insert(i,href)
               i=i+1
+
+      choice = int(raw_input("Enter your choice of video or press -1 "))
+      select =list1[choice]
+      link = "https://www.youtube.com"+select
       
-      choice = int(raw_input("Want to play video enter your choice else -1 "))
-      select = list1[choice]
-      link = "https://www.youtube.com" + select
+      if choice!=-1:
+         option = raw_input("VLC(v) or Youtube(y) ")
+         if option=='y':
+            webbrowser.open(link)
+         else:
+            myprocess = subprocess.call(['vlc','-vvv',link])
+      choice=raw_input("Want to continue y/n ")
+      if choice=='n':
+         con=False
+      i=0   
